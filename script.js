39094fa9ee6b0b0efbdadc34d59c575f373a81fa
Vaishnavi-0304 <vaishnavi.jadhav2409@gmail.com>
2024-09-27 21:30:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-document.querySelectorAll('.dropdown-toggle').forEach(item => {
-    item.addEventListener('click', event => {
-        const dropdown = item.nextElementSibling;
-        dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-    });
-});
-
-// Close dropdowns if clicked outside
-window.addEventListener('click', event => {
-    if (!event.target.matches('.dropdown-toggle')) {
-        document.querySelectorAll('.dropdown').forEach(dropdown => {
-            dropdown.style.display = 'none';
-        });
-    }
-});
+ $(document).ready(function(){
+  $('.toggle ').click(function(){
+   $('.toggle').toggleClass('active')
+   $('.menu').toggleClass('active')
+  })
+ });
